8da74264aaabcf8c262ee75859e4863cc73d415c
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2022-05-15 18:45:53 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=895284fb2d2c50a520ea537456963d9c`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
+      </div>
+   
+      </div>
+    </div>
+  );
+}
+
+export default App;
+// npm i axios 
+// ApenWeathermap.org is the API that I'm gonna use
+
