7aed94e707464bd976c9a2aa8568a8c8cda22a4f
Roberto Teixeira <robertoteixeira.developer@gmail.com>
2022-05-15 18:47:36 -0300

    humidity and wind

--- src/App.js
+++ src/App.js
@@ -40,6 +40,22 @@ function App() {
           </div>
         </div>
 
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
       </div>
     </div>
   );
